4c635a358e51fe96c27416c70b057562c3a0d6dc
boboyevnurbek <boboyevnurbek95@gmail.com>
2023-06-21 09:46:01 +0500

    COLLECTIONS -> FINAL TASK

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+
+        projects.forEach((project, developers) -> {
+            if (developers.contains(developer)) {
+                list.add(project);
+            }
+        });
+
+        Comparator<String> stringComparator = (s1, s2) -> {
+            if(s1.length() == s2.length()){
+                return s2.compareTo(s1);
+            }
+            return Integer.compare(s2.length(), s1.length());
+        };
+
+        list.sort(stringComparator);
+
+        return list;
     }
 }
